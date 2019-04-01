dae05b57e008b33e8c83270982c091617655f06a
collins njau <collinsnjau39@gmail.com>
2019-04-01 10:49:56 +0300

    Add guzzle for http requests

--- app/Console/Commands/newsData.php
+++ app/Console/Commands/newsData.php
@@ -3,6 +3,8 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Client;
 use App\News;
 class newsData extends Command
 {
@@ -37,15 +39,19 @@ class newsData extends Command
      */
     public function handle()
     { 
-       News::Create([
-               'url' => 'www.trio.com',
-               'urlToImage'=>'www.trio.com/image',
-               'content' => 'charles barkley',
-               'author' => 'john Doe',
-               'source' => 'bbc',
-               'title'=> 'Charles Barkley shocked fans',
-               'publishedAt' => '2019-03-28T15:17:46Z'
-        ]);
+       $client = new Client();
+       // make api call to news api
+       $result = $client->get('https://newsapi.org/v2/everything?q=kenya&apiKey=f6dce1d67d0a4e94a03a795bb892c499');
+       echo $result->getBody();
+    //    News::Create([
+    //            'url' => 'www.trio.com',
+    //            'urlToImage'=>'www.trio.com/image',
+    //            'content' => 'charles barkley',
+    //            'author' => 'john Doe',
+    //            'source' => 'bbc',
+    //            'title'=> 'Charles Barkley shocked fans',
+    //            'publishedAt' => '2019-03-28T15:17:46Z'
+    //     ]);
         echo 'Added successfully';
     }
 }
